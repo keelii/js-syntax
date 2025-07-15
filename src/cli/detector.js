a1f620728c2d0c88130b48e586ad8f1308771cb6
keelii <keeliizhou@gmail.com>
2025-07-15 14:08:28 +0800

    feat: new output.

---
+++ src/cli/detector.js
@@ -0,0 +1,24 @@
+import {detect} from "../lib/detect";
+
+export function detectText(text, { file, opts }) {
+    var ret = detect(text)
+
+    if (opts.V) {
+        console.log(`[➤] Detected syntax(${text.length}):\n`, text.slice(0, 77) + (text.length > 77 ? "..." : ""));
+    }
+
+    for (let key in ret) {
+        var item = ret[key]
+        for (let i = 0; i < item.locations.length; i++) {
+            console.log(`[⚠] <${key}> ${file}:${item.locations[i].start.line}:${item.locations[i].start.column}`);
+        }
+    }
+
+    var results = Object.values(ret).flat()
+    if (results.length < 1) {
+        console.log("[✓] No newer syntax in", file);
+        return 0
+    } else {
+        return results.length
+    }
+}
--- src/cli/js-syntax.js
+++ src/cli/js-syntax.js
@@ -1,27 +1,32 @@
-import path from "tjs:path";
 import getopts from "tjs:getopts";
-import {detect} from "../lib/detect.js";
 import {decodeText, isURL, readStdin} from "./utils.js";
 import packageJson from "../../package.json" with { type: "json" };
-import {arrayBufferToFile} from "../lib/stream";
 import {doUpgrade} from "./upgrade";
+import {detectText} from "./detector";
+
 
 var opts = getopts(tjs.args, {
     alias: {
         u: "upgrade",
         h: "help",
-        v: "version"
+        V: "version",
+        v: "verbose",
     }
 })
 
 var document = `Usage: js-syntax [options] <file|url>
   -u, --upgrade            Do upgrading js-syntax itself
   -h, --help               Show this help message and exit
-  -v, --version            Show version number and exit
+  -v, --verbose            Show detailed information
+  -V, --version            Show version number and exit
 Example:
   js-syntax --upgrade
   js-syntax https://example.com/script.js
   js-syntax /path/to/local/script.js`;
+
+if (opts.V) {
+    console.log(opts)
+}
 
 if (opts._.includes("tjs") && opts._.includes("run")) {
     opts._ = opts._.slice(3);
@@ -40,7 +45,7 @@ if (opts.v) {
 }
 if (opts.u) {
     if (tjs.system.platform === "windows") {
-        console.error("Error: Upgrading js-syntax is not supported on Windows platform.");
+        console.error("[✘] Error: Upgrading js-syntax is not supported on Windows platform.");
         tjs.exit(1);
     } else {
         await doUpgrade(packageJson.version)
@@ -48,47 +53,55 @@ if (opts.u) {
     }
 }
 
-var text = ""
-var file = ""
-
 if (args.length < 1) {
     if (tjs.stdin.type === "pipe") {
-        file = "stdin"
-        text = await readStdin()
+        var text = await readStdin()
+        var res = detectText(text, { file: "stdin", opts })
+        if (res) {
+            console.log(`[⚠] Found ${res} newer syntax(ES6+) in stdin.`);
+            tjs.exit(1);
+        } else {
+            console.log("[✓] No newer syntax in stdin.");
+            tjs.exit(0);
+        }
     } else {
         console.log(document)
     }
 }
 
-if (!text) {
+let newerSyntaxCount = 0
+for (const arg of args) {
     try {
-        if (isURL(args[0])) {
-            var response = await fetch(args[0])
-            text = await response.text()
+        if (opts.V) {
+            console.log("[➤] Get file:", arg);
+        }
+        if (isURL(arg)) {
+            var response = await fetch(arg)
+            var text = await response.text()
             if (text.trim() === "") {
-                console.error("Error: The URL returned an empty response.");
+                console.error("[✘] Error: The URL returned an empty response.");
                 tjs.exit(1);
             }
-            file = args[0]
+            var yes = detectText(text, { file: arg, opts });
+            if (yes) newerSyntaxCount += yes
         } else {
-            var res = await tjs.readFile(args[0])
-            text = decodeText(res)
-            file = path.resolve(args[0])
+            var res = await tjs.readFile(arg)
+            var text = decodeText(res)
+            if (text.trim() === "") {
+                console.error("[✘] Error: The file content empty.");
+                tjs.exit(1);
+            }
+            var yes = detectText(text, { file: arg, opts });
+            if (yes) newerSyntaxCount += yes
         }
     } catch (e) {
-        console.error("Error reading file or URL:", e);
+        console.error("[✘] Error reading file or URL:", e);
         tjs.exit(1);
     }
 }
 
-var ret = detect(text)
-for (let key in ret) {
-    var item = ret[key]
-    for (let i = 0; i < item.locations.length; i++) {
-        console.log(`[${key}] ${file}:${item.locations[i].start.line}:${item.locations[i].start.column}`);
-    }
-}
-
-if (Object.values(ret).flat().length < 1) {
-    console.log("No newer syntax(ES6+) syntax found.")
+// console.log(newerSyntaxCount)
+if (newerSyntaxCount > 1) {
+    console.log(`[⚠] Found ${newerSyntaxCount} newer syntax(ES6+) in the files.`);
+    tjs.exit(1);
 }
